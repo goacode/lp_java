39b02de4ce1db721d3d94d814a5dca708ad203b9
goacode <trabalhogustavoandrade@gmail.com>
2026-02-23 20:22:13 -0300

    EST_DEC_19 CORRIGIDO

--- src/EST_DEC_19.java
+++ src/EST_DEC_19.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class EST_DEC_19 {
+/*
+Objetivo: Receba 2 valores reais. Calcule e mostre o maior deles.
+Dev: Gustavo O. Andrade
+Data: 23/02/2026
+ */
+import javax.swing.JOptionPane;
+public class EST_DEC_19 {
+    public static void main(String args[]){
+        double valor1 = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro valor"));
+        double valor2 = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo valor"));
+
+        if(valor1 > valor2){
+            JOptionPane.showMessageDialog(null,"O maior valor é " + valor1);
+        }else if(valor2 > valor1){
+            JOptionPane.showMessageDialog(null, "O maior valor é " + valor2);
+        }else{
+            JOptionPane.showMessageDialog(null, "Ambos valores são iguais.");
+        }
+    }
 }
